a27a7a8d2ea2b698255922b3b873c5a93fdc0a8f
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-05-29 21:48:59 +0700

    mulai koding di ListPresidentAdapter.java

--- app/src/main/java/com/takatutustudio/myrecyclerview/ListPresidentAdapter.java
+++ app/src/main/java/com/takatutustudio/myrecyclerview/ListPresidentAdapter.java
@@ -1,4 +1,6 @@
 package com.takatutustudio.myrecyclerview;
 
-public class ListPresidentAdapter {
+import android.support.v7.widget.RecyclerView;
+
+public class ListPresidentAdapter extends RecyclerView.Adapter<ListPresidentAdapter.CategoryViewHolder> {
 }
